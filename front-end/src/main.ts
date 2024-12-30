9f205407f7a85de6d52910a3e260b2a18297ed48
رضا پورشریعتی <rpourshariati@noornent.net>
2024-12-30 14:10:57 +0330

    added prime vue and tailwind css

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './assets/main.css'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
@@ -9,5 +10,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  unstyled: true,
+})
 
 app.mount('#app')
